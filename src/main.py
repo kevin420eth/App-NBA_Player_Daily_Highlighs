b25877749059a9a7d68e153df6466ca8ed3158a1
Kevin Mars <kevin.eth.420@gmail.com>
2023-03-30 13:40:32 +0800

    normal modified

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
 
     # Build the command to launch Chrome in debug mode
     cmd_open_browser = f'{chrome_path} --remote-debugging-port={remote_debugging_port} --user-data-dir="c:/selenum/automationprofile"'
-    time.sleep(1)
+    time.sleep(10)
     cmd_open_new_tab = f'{chrome_path} --remote-debugging-port={remote_debugging_port} --user-data-dir="c:/selenum/automationprofile" --new-tab "https://www.google.com/"'
 
     # Launch Chrome in debug mode
